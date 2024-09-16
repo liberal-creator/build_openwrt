55cae31aa7504e2812eef64ecb1aa8299c33e5b9
liberal-creator <86588725+liberal-creator@users.noreply.github.com>
2024-09-16 19:08:30 +0800

    Update 01_prepare_source_code.sh

--- scripts/immortalwrt-23.05/01_prepare_source_code.sh
+++ scripts/immortalwrt-23.05/01_prepare_source_code.sh
@@ -16,7 +16,7 @@ wait
 
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' openwrt/package/base-files/files/etc/shadow
-# Modify default IP (FROM 192.168.1.1 CHANGE TO 192.168.1.99 )
-sed -i 's/192.168.1.1/192.168.1.99/g' openwrt/package/base-files/files/bin/config_generate
+# Modify default IP (FROM 192.168.1.1 CHANGE TO 192.168.1.3 )
+sed -i 's/192.168.1.1/192.168.1.3/g' openwrt/package/base-files/files/bin/config_generate
 
 exit 0
